2453cd7eea6dd4fb546ee4d24545e34fa881a10f
Connor Richter <Connorrichter4@gmail.com>
2021-01-02 11:31:47 -0600

    initializing git repository and added Create and Read from file

---
+++ src/main/java/fileio/App.java
@@ -0,0 +1,14 @@
+package fileio;
+
+import java.io.IOException;
+
+public class App {
+
+	public static void main(String[] args) throws IOException {
+
+		CreateAndModify newFile = new CreateAndModify();
+		newFile.readFromFile();
+		
+	}
+
+}
---
+++ src/main/java/fileio/CreateAndModify.java
@@ -0,0 +1,55 @@
+package fileio;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class CreateAndModify {
+
+	final static String filePath = "src/main/resources/testFile.txt";
+
+	private FileOutputStream fileOutputStream;
+
+	public CreateAndModify() {
+		try {
+			File file = new File(filePath);
+
+			if (!file.exists()) {
+				System.out.println("File is created");
+				Files.createFile(Paths.get(filePath));
+				FileWriter writer = new FileWriter(file);
+				writer.write("Test Data\n");
+				writer.close();
+
+			} else {
+				System.out.println("File already exists");
+			}
+
+			this.fileOutputStream = new FileOutputStream(file, true);
+//			String testData = "Hello World\n";
+//			fileOutputStream.write(testData.getBytes());
+//			fileOutputStream.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Unable to create debug log file");
+		}
+	}
+
+	public void readFromFile() throws IOException {
+		File readFile = new File(filePath);
+		FileInputStream fileInputStream = new FileInputStream(readFile);
+		int r = 0;
+		StringBuilder stringBuilder = new StringBuilder();
+		while ((r = fileInputStream.read()) != -1) {
+			stringBuilder.append((char) r);
+		}
+		String inputFile = stringBuilder.toString();
+		System.out.println(inputFile);
+	}
+
+}
